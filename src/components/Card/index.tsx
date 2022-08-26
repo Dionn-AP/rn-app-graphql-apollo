595e9e90222b0d6cc171bacbbe19fdda290b2a58
Dionnatan Alves <dionn_a.p@hotmail.com>
2022-08-26 14:38:27 -0300

    my first commit

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,31 @@
+import {
+    View,
+    Text,
+    Image
+} from 'react-native';
+
+import styles from './styles';
+
+import { IProps } from '../../type';
+
+export default function Card({ card }: IProps) {
+    return (
+        <View
+            style={styles.card}
+        >
+            <Image
+                source={{ uri: card.image }}
+                style={styles.image}
+            />
+            <View
+                style={styles.imageDescriptionContainer}
+            >
+                <Text
+                    style={styles.text}
+                >
+                    {`${card.name}, ${card.gender}`}
+                </Text>
+            </View>
+        </View>
+    )
+}
